1a2912dc5535ce26c804d3b5875716d94eb8267c
Falcotton <fal19cotton@gmail.com>
2020-08-13 23:08:02 +0900

    11-5

---
+++ Chapter11/text.hs
@@ -0,0 +1,85 @@
+import Data.Char
+import Data.List
+import System.IO
+
+size :: Int
+size = 3
+
+type Grid = [[Player]]
+
+data Player = O | B | X
+  deriving (Eq,Ord,Show)
+
+next :: Player -> Player
+next O = X
+next B = B
+next X = O
+
+
+empty :: Grid
+empty = replicate size (replicate size B)
+
+full :: Grid -> Bool
+full = all (/= B) . concat
+
+turn :: Grid -> Player
+turn g = if os <= xs then O else X
+            where
+              os = length (filter (== O) ps)
+              xs = length (filter (== X) ps)
+              ps = concat g
+
+wins :: Player -> Grid -> Bool
+wins p g = any line (rows ++ cols ++ dias)
+            where
+              line = all (== p)
+              rows = g
+              cols = transpose g
+              dias = [diag g, diag (map reverse g)]
+
+-- (!!) [1,2,3] !! 1 = 2
+-- １行目２行目３行目
+-- [g !! n | n <- [0..size-1]]
+-- １行目の１つ目、２行目の２つ目、３行目の３つ目
+-- [g !! n !! n | n <- [0..size-1]]
+diag :: Grid -> [Player]
+diag g = [g !! n !! n | n <- [0..size-1]]
+
+won :: Grid -> Bool
+won g = wins O g || wins X g
+
+
+putGrid :: Grid -> IO ()
+putGrid =
+  putStrLn . unlines . concat . interleave bar . map showRow
+    where
+      bar = [replicate ((size*4)-1) '-']
+
+showRow :: [Player] -> [String]
+showRow = beside . interleave bar . map showPlayer
+  where
+    beside  = foldr1 (zipWith (++))
+    bar     = replicate 3 "|"
+
+showPlayer :: Player -> [String]
+showPlayer O = ["   "," O ","   "]
+showPlayer B = ["   ","   ","   "]
+showPlayer X = ["   "," X ","   "]
+
+interleave :: a -> [a] -> [a]
+interleave x []     = []
+interleave x [y]    = [y]
+interleave x (y:ys) = y : x : interleave x ys
+
+valid :: Grid -> Int -> Bool
+valid g i = 0 <= i && i < size^2 && concat g !! i == B
+
+move :: Grid -> Int -> Player -> [Grid]
+move g i p = 
+  if valid g i then [chop size (xs ++ [p] ++ ys)] else []
+  where
+    (xs,B:ys) = splitAt i (concat g)
+
+chop :: Int -> [a] -> [[a]]
+chop n [] = []
+chop n xs = take n xs : chop n (drop n xs)
