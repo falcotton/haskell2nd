6112e11f945c822265482be15484ca0b81fbcb4e
Falcotton <fal19cotton@gmail.com>
2020-09-27 14:06:30 +0900

    Chapter12 textbook

--- Chapter12/textbook.hs
+++ Chapter12/textbook.hs
@@ -1,5 +1,6 @@
-data Tree a = Leaf a | Node (Tree a) (Tree a)
-  deriving Show
+import Data.Char
+-- data Tree a = Leaf a | Node (Tree a) (Tree a)
+--   deriving Show
 
 instance Functor Tree where
   fmap g (Leaf x)   = Leaf (g x)
@@ -23,3 +24,81 @@ eval (Div x y)  = case eval x of
                     Just n  -> case eval y of
                                 Nothing -> Nothing
                                 Just m  -> safediv n m
+
+
+
+
+type State = Int
+-- type ST a = State -> (a,State)
+newtype ST a = S (State -> (a,State))
+
+app :: ST a -> State -> (a,State)
+app (S st) x = st x
+
+instance Functor ST where
+  -- fmap :: (a -> b) -> ST a -> ST b
+  fmap g st = S(\s -> let (x,s') = app st s in (g x, s'))
+
+instance Applicative ST where
+  -- pure :: a -> ST a
+  pure x = S (\s -> (x,s))
+
+  -- (<*>) :: ST (a -> b) -> ST a -> ST b)
+  stf <*> stx = S (\s ->
+    let   (f,s')  = app stf s
+          (x,s'') = app stx s' in (f x,s''))
+
+instance Monad ST where
+  -- (>==) :: ST a -> (a -> ST b) -> ST b
+  st >>= f = S(\s -> let (x,s') = app st s in app(f x) s')
+
+
+
+data Tree a = Leaf a | Node (Tree a) (Tree a)
+  deriving Show
+
+tree :: Tree Char
+tree = Node (Node (Leaf 'a') (Leaf 'b'))(Leaf 'c')
+
+rlabel :: Tree a -> Int -> (Tree Int,Int)
+rlabel (Leaf _)  n  = (Leaf n,n+1)
+rlabel (Node l r) n = (Node l' r',n'')
+                      where
+                        (l',n')   = rlabel l n
+                        (r',n'')  = rlabel r n'
+
+
+fresh  :: ST Int
+fresh = S(\n -> (n,n+1))
+
+alabel :: Tree a -> ST (Tree Int)
+alabel (Leaf _)   = Leaf <$> fresh
+alabel (Node l r) = Node <$> alabel l <*> alabel r
+
+mlabel :: Tree a -> ST (Tree Int)
+mlabel (Leaf _) = do  n <- fresh
+                      return (Leaf n)
+mlabel (Node l r) = do  l' <- mlabel l
+                        r' <- mlabel r
+                        return (Node l' r')
+
+mapM :: Monad m => (a -> m b) -> [a] -> m [b]
+mapM f [] = return []
+mapM f (x:xs) = do  y <- f x
+                    ys <- Main.mapM f xs
+                    return (y:ys)
+
+conv :: Char -> Maybe Int
+conv c  | isDigit c = Just (digitToInt c)
+        | otherwise = Nothing 
+
+filterM :: Monad m => (a -> m Bool) -> [a] -> m [a]
+filterM p [] = return []
+filterM p (x:xs) = do b <- p x
+                      ys <- Main.filterM p xs
+                      return (if b then x:ys else ys)
+
+join :: Monad m => m(m a) -> m a
+join mmx = do mx <- mmx
+              x <- mx
+              return x
